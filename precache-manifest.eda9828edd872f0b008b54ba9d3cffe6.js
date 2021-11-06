97a0b30cfeffe2206a83256a1783f70cbf6d4710
kcheung3102 <kcheung3102@gmail.com>
2021-11-06 01:02:44 -0400

    Updates

---
+++ precache-manifest.eda9828edd872f0b008b54ba9d3cffe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efeeb72294fe8de89a771341ed2d282",
+    "url": "/cinema-cloud/index.html"
+  },
+  {
+    "revision": "e159f84e00894937b1ed",
+    "url": "/cinema-cloud/static/css/main.9f4e7461.chunk.css"
+  },
+  {
+    "revision": "a9c846233116f8956a17",
+    "url": "/cinema-cloud/static/js/2.10279b53.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cinema-cloud/static/js/2.10279b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e159f84e00894937b1ed",
+    "url": "/cinema-cloud/static/js/main.4aa7eada.chunk.js"
+  },
+  {
+    "revision": "d65045ec3a8463b7b5f3",
+    "url": "/cinema-cloud/static/js/runtime-main.1d7d157c.js"
+  },
+  {
+    "revision": "746db003bad9088e108e516076a15bbe",
+    "url": "/cinema-cloud/static/media/cloud.746db003.png"
+  }
+]);
